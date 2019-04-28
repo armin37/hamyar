3d12d9f8935dc0ec8894c43c49214cd72bd01b84
acer <alimahmudvand@yahoo.com>
2019-04-28 07:51:20 +0430

    add login , verfy, signup,home, personal api and storage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,19 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {PersonalPage} from "../personal/personal";
+import {Storage} from "@ionic/storage";
+import {HttpApiProvider} from "../../providers/http-api/http-api";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  posts:any=[];
+  constructor(
+    public navCtrl: NavController
+    , private storage: Storage
+    , public httpApi: HttpApiProvider
+  ) {
 
   }
+  ionViewDidLoad() {
+    this.getPostList();
+  }
 
-  public goToPersonal()
-  {
-    this.navCtrl.push(PersonalPage);
+  public goToPersonal(phoneNumber){
+    this.navCtrl.push(PersonalPage,{
+      phoneNumber
+    });
+  }
+
+  getPostList=async()=> {
+    let res:any=await this.httpApi.sendPostRequest("/post/list")
+    if(Array.isArray(res)){
+      this.posts=res;
+    }
   }
 }
--- src/pages/personal/personal.ts
+++ src/pages/personal/personal.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {HttpApiProvider} from "../../providers/http-api/http-api";
 
 /**
  * Generated class for the PersonalPage page.
@@ -13,12 +14,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'personal.html',
 })
 export class PersonalPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private phoneNumber: any;
+  private user: any={};
+  private posts: any=[];
+  constructor(
+    public navCtrl: NavController,
+   public httpApi: HttpApiProvider,
+  public navParams: NavParams) {
+    this.phoneNumber=navParams.get('phoneNumber');
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PersonalPage');
+    this.getUserProfile();
+  }
+  getUserProfile=async()=> {
+    let res:any=await this.httpApi.sendPostRequest("/profile",{
+      phoneNumber:this.phoneNumber
+    })
+    if(Array.isArray(res)){
+      this.user=res[0];
+      this.posts=res[1];
+    }
   }
 
 }
