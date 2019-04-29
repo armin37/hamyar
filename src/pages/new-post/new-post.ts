fecdea8aaeaf66e1c0696c251ac7600b207625d6
acer <alimahmudvand@yahoo.com>
2019-04-29 06:22:05 +0430

    add upload post image

--- src/pages/new-post/new-post.ts
+++ src/pages/new-post/new-post.ts
@@ -3,6 +3,7 @@ import {IonicPage, LoadingController, NavController, NavParams} from 'ionic-angu
 import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
 import {Storage} from "@ionic/storage";
+import {Camera,CameraOptions} from "@ionic-native/camera";
 import {HttpApiProvider} from "../../providers/http-api/http-api";
 
 /**
@@ -21,6 +22,8 @@ export class NewPostPage {
   text: AbstractControl;
   image:string;
   video:string;
+  private base64Image: string="";
+  private imageSrc: any=null;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
@@ -28,6 +31,7 @@ export class NewPostPage {
     , public httpApi: HttpApiProvider
     , @Inject('ApiBaseUrl') private apiBaseUrl: string
     , private http: HttpClient
+    , private camera: Camera
     , private storage: Storage) {
     this.myForm = formBuilder.group({
       'text': ['', Validators.compose([
@@ -46,14 +50,32 @@ export class NewPostPage {
   close(){
     this.navCtrl.pop();
   }
+  takePhoto(sourceType:number) {
+    const options: CameraOptions = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true,
+      sourceType:sourceType,
+    }
 
+    this.camera.getPicture(options).then((imageData) => {
+      // this.imageSrc = 'data:image/jpeg;base64,' + imageData;
+      this.base64Image = /*'data:image/jpeg;base64,' +*/ imageData;
+    }, (err) => {
+      // Handle error
+    });
+  }
   newPost=async()=> {
 
     let user:any=await this.storage.get("user");
     let content:any=this.myForm.value.text;
+    let photo:any=this.base64Image;
     let res=await this.httpApi.sendPostRequest("/post/create",{
       content,
-      userId:user._id
+      userId:user._id,
+      photo
     })
     if(res){
       this.navCtrl.pop();
